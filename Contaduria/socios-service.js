7473776ad8390008d9ef87e4f13f4b9b91f27259
GuancoBoleado <131415990+GuancoBoleado@users.noreply.github.com>
2026-04-13 21:49:29 -0300

    socios-service.js

---
+++ Contaduria/socios-service.js
@@ -0,0 +1,20 @@
+import { supabase } from './base-de-datos.js';
+
+export async function obtenerSocios() {
+    const { data, error } = await supabase
+        .from('asociados')
+        .select('*')
+        .order('apellido', { ascending: true });
+    
+    if (error) throw error;
+    return data;
+}
+
+export async function registrarSocio(socio) {
+    const { data, error } = await supabase
+        .from('asociados')
+        .insert([socio]);
+    
+    if (error) throw error;
+    return data;
+}
