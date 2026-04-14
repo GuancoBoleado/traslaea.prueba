ec500591b51bc583686365f44778c505ad909e40
GuancoBoleado <131415990+GuancoBoleado@users.noreply.github.com>
2026-04-13 22:48:18 -0300

    base-de-datos.js

--- Contaduria/base-de-datos.js
+++ Contaduria/base-de-datos.js
@@ -3,9 +3,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 // Configuración de conexión con tu proyecto de Supabase
 const SUPABASE_URL = 'https://otpsokpojwiebfkrjrtx.supabase.co'; 
 
-// 1. Buscá en Supabase (Settings -> API) la que dice "anon" "public"
-// 2. Copiá ese código largo que empieza con eyJ...
-// 3. Pegalo acá adentro de las comillas:
-const SUPABASE_KEY = 'PEGÁ_ACÁ_TU_LLAVE_ANON_PÚBLICA'; 
+// Tu llave pública actualizada (Publishable key)
+const SUPABASE_KEY = 'sb_publishable_ewz7_msS7Ch1DCWGLOYCeg_wvVcTGCT'; 
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
