0669a133c41088e2f601a47a9cd166c43bb6ed03
GuancoBoleado <131415990+GuancoBoleado@users.noreply.github.com>
2026-04-13 21:59:39 -0300

    base-de-datos.js

--- base-de-datos.js
+++
@@ -1,9 +0,0 @@
-// base-de-datos.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-// --- REEMPLAZA ESTOS DATOS CON LOS TUYOS ---
-const SUPABASE_URL = "https://TU_PROYECTO.supabase.co"; 
-const SUPABASE_KEY = "TU_CLAVE_ANON_LARGA_AQUÍ"; 
-// -------------------------------------------
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
