1077eab8cb4d5d3c0503e5c5761cc7559687466a
GuancoBoleado <131415990+GuancoBoleado@users.noreply.github.com>
2026-09-20 23:24:22 -0300

    kiri-config.js
    Updated KIRI_CONFIG with a new API_KEY and added MODEL property.

--- traslaea-web/js/kiri-config.js
+++ traslaea-web/js/kiri-config.js
@@ -1,3 +1,5 @@
-window.KIRI_CONFIG = { 
-    API_KEY: "AQ.Ab8RN6LRMh_YCkdwlriftJ9w5vF899fkaNpjP7bdgNpo4WCayA"
+// --- KIRI: Configuración ---
+const KIRI_CONFIG = {
+    API_KEY: "AQ.Ab8RN6KhtYOZoIXqyQhIN0bLpFOiIB8ljAHI8NCS_AU4dkiCsQ",
+    MODEL: "gemini-flash-latest" // O "gemini-2.5-flash" según prefieras
 };
