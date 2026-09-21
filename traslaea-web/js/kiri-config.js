52dd7e463f4ecb15beee5959fdc6aed8c3f0dc8e
GuancoBoleado <131415990+GuancoBoleado@users.noreply.github.com>
2026-09-20 22:14:11 -0300

    kiri-config.js

--- traslaea-web/js/kiri-config.js
+++ traslaea-web/js/kiri-config.js
@@ -1,1 +1,3 @@
-window.KIRI_ENV_API_KEY = "AQ.Ab8RN6LRMh_YCkdwlriftJ9w5vF899fkaNpjP7bdgNpo4WCayA" };
+window.KIRI_CONFIG = {
+    API_KEY: "AQ.Ab8RN6LRMh_YCkdwlriftJ9w5vF899fkaNpjP7bdgNpo4WCayA"
+};
