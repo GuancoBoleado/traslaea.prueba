2a7c5262001cf5a2e71b8cb36d5ff584ee267310
GuancoBoleado <131415990+GuancoBoleado@users.noreply.github.com>
2026-09-20 23:48:31 -0300

    kiri-chat.js

--- traslaea-web/js/kiri-chat.js
+++ traslaea-web/js/kiri-chat.js
@@ -189,7 +189,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 <div class="kiri-chatbot-title">
                     <img src="Kiri CHAT.jpeg" alt="KIRI" onerror="this.src='traslaea-web/assets/img/noticia1.jpg'">
                     <div>
-                        <h4>KIRI</h4>
+                        <h4>KIRI BOT</h4>
                         <span>● Tu Asistente Técnico</span>
                     </div>
                 </div>
